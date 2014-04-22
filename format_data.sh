face84e81a8598cad7b81f98499545c1955b83fb
Colleen Murphy <c.murphy@pdx.edu>
2014-04-21 23:11:18 -0700

    Add data formatting script

---
+++ format_data.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+STREAMS="1 2"
+
+# Frames/Viewing Order vs Size in Bits
+for i in $STREAMS
+do
+  for j in `seq 0 7`
+  do
+    echo "#Frame Type  Viewing Order  PSNR(Y)  Size" > plotting/${i}/stream${j}.dat
+    sed 's/              / ... ........ /' streams/logs/${i}/stream${j}.log | sed 's/          / ........ /' | sed 's/     / ... /' | awk '/ViewCodingOrder/{flag=1; next} /^$/{flag=0} flag { printf "%-12s %-14s %-8s %-14s\n", $2, $5, $9, $18 }' | sort -nk 2 >> plotting/${i}/stream${j}.dat;
+  done
+done
