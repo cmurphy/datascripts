78bf3bacae1406a20b8724c808402c3e38d4578e
Colleen Murphy <c.murphy@pdx.edu>
2014-05-08 13:23:51 -0700

    Usability changes
    Added comments and documentation.
    Format script now accepts arguments for what streams to use.
    Directory structure now the same for all scripts.

--- format_data.sh
+++ format_data.sh
@@ -1,13 +1,22 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-STREAMS="1 2 3 4"
+# This script must be run before either the plot.gpp or aggr_plot.gpp scripts are run.
+
+# Assumes there is a ./streams/logs accessible from wherever this script is run
+LOG_DIR=streams/logs
+
+# Run this script like (for example):
+#   ./format_data.sh 1 2 3 4 5 6 7
+# to format data from trials 1 through 7
+STREAMS="$@"
 
 # Frames/Viewing Order vs Size in Bits
 for i in $STREAMS
 do
-  for j in `seq 0 7`
+  mkdir -p plotting/${i}
+  for j in `seq 0 7`      # Assumes 8 streams
   do
     echo "#Frame Type  Viewing Order  PSNR(Y)  Size" > plotting/${i}/stream${j}.dat
-    sed 's/              / ... ........ /' streams/logs/${i}/stream${j}.log | sed 's/          / ........ /' | sed 's/     / ... /' | awk '/ViewCodingOrder/{flag=1; next} /^$/{flag=0} flag { if ($4 == "REF_VIEW") printf "%-12s %-14s %-8s %-14s %-8s\n", $2, $5, $9, $18, $4; else printf "%-12s %-14s %-8s %-14s\n", $2, $5, $9, $18; }' | sort -nk 2 >> plotting/${i}/stream${j}.dat;
+    sed 's/              / ... ........ /' ${LOG_DIR}/${i}/stream${j}.log | sed 's/          / ........ /' | sed 's/     / ... /' | awk '/ViewCodingOrder/{flag=1; next} /^$/{flag=0} flag { if ($4 == "REF_VIEW") printf "%-12s %-14s %-8s %-14s %-8s\n", $2, $5, $9, $18, $4; else printf "%-12s %-14s %-8s %-14s\n", $2, $5, $9, $18; }' | sort -nk 2 >> plotting/${i}/stream${j}.dat;
   done
 done
